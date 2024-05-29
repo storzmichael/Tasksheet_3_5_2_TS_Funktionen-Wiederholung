a4f8b2fc33882e0cb0881487c4c4d15a989d30cf
storzmichael <micsto24@gmail.com>
2024-05-29 08:57:56 +0200

    add more functions

--- function.dart
+++ function.dart
@@ -1,4 +1,7 @@
 //  Hello World
+import 'dart:io';
+import 'dart:math';
+
 void helloWorld() {
   print('Hello World!');
 }
@@ -82,12 +85,81 @@ int multiMoreNumbers(int firstNumber,int secondNumber,int thirdNumber) {
   return multiNumbers(firstNumber,secondNumber) * thirdNumber;
 }
 
-// Vorzeichen umdrehen
+//  Vorzeichen umdrehen
 int aroundSign (int number){
   return (number - (number*2));
 }
 
-//Finde die kleinste Zahl
+//  Finde die kleinste Zahl
 int smallNumber(int firstNumber, int secondNumber) {
   return (firstNumber > secondNumber) ? secondNumber : firstNumber;
 }
+
+//  Umrechnen von Temperatur
+String convertTemperature(num temperature, String unit){
+  if (unit == 'C'){
+    num resultInF = (temperature * 1.8 + 32);
+    String resultInFStr = resultInF.toStringAsFixed(1);
+    return ('Die Temperatur $temperature C° sind $resultInFStr F.');
+  } else {
+    num resultInC = ((temperature - 32) / 1.8);
+    String resultInCStr = resultInC.toStringAsFixed(1);
+    return ('Die Temperatur $temperature F sind $resultInCStr C°.');
+  }
+}
+
+//  Zeichenketten-Verkettung
+void concatenation(List<String> letterList){
+  for (String letter in letterList){
+    stdout.write(letter); 
+  }
+}
+
+//  Prime Checker
+bool isPrime(int number) {
+  if (number <= 1) return false;
+  if (number == 2) return true;
+  if (number % 2 == 0) return false;
+
+  
+  for (int i = 3; i <= number ~/ 2; i += 2) {
+    if (number % i == 0) return false;
+  }
+
+  return true;
+}
+
+//  Reverse Number 
+int reverseNumber(int number) {
+  int reversed = 0;
+  
+  while (number > 0) {
+    int digit = number % 10;  // Extrahiere die letzte Ziffer
+    reversed = reversed * 10 + digit;  // Füge die Ziffer zur umgekehrten Zahl hinzu
+    number ~/= 10;  // Entferne die letzte Ziffer von der Originalzahl
+  }
+  
+  return reversed;
+}
+
+// Seconds Formatter
+String secondsFormatter(int inputSeconds){
+  int seconds = inputSeconds % 60;
+  int totalMinutes = inputSeconds ~/ 60;
+  int minutes = totalMinutes % 60; 
+  int hours = totalMinutes ~/ 60;
+  return ('$hours Stunden $minutes Minuten $seconds Sekunden');
+}
+
+//  Anagram
+bool areAnagrams (String word1, String word2){
+  if (word1.length != word2.length){
+    return false;
+}
+  List<String> sortedWord1 = word1.split('')..sort();
+  List<String> sortedWord2 = word2.split('')..sort();
+
+  return sortedWord1.join() == sortedWord2.join();
+}
+
+//  Multiplication Advanced
