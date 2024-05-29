26ba0367e9e59710b18de7c054a390bd268d9975
storzmichael <micsto24@gmail.com>
2024-05-29 14:04:18 +0200

    commit

--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
 import 'function.dart';
 
+
 void main() {
   List<String> names = ['Michael', 'Jan', 'Simon', 'Ibrahim'];
   Map<String, int> result = numberOfletters(names);
@@ -15,5 +16,32 @@ void main() {
 
   print(smallNumber(4, 3));
 
+  print(convertTemperature(29, 'C'));
 
-}
+  print(convertTemperature(84, 'F'));
+
+  concatenation(names);
+
+  print('');
+
+  print(isPrime(7));
+
+  print(reverseNumber(789));
+
+  print(secondsFormatter(3661));
+
+  print(areAnagrams('ort', 'rot'));
+
+  squarePattern(4, 4);
+
+  print(divideIntoWords('Hallo meine Name ist Michael'));
+
+  print(countCharacters('Hallo mein Name ist Michael!'));
+  
+  print(checkBrackets("((())()"));
+  }
+
+
+
+
+
